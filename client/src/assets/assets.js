79d5a3f13b9794136940c68300ffcbc23d7be857
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-04-12 21:28:26 +0530

    Add new assets: profile image, search icon, and send button SVG

---
+++ client/src/assets/assets.js
@@ -0,0 +1,140 @@
+import avatar_icon from './avatar_icon.png'
+import gallery_icon from './gallery_icon.svg'
+import help_icon from './help_icon.png'
+import logo_icon from './logo_icon.svg'
+import logo_big from './logo_big.svg'
+import logo from './logo.png'
+import profile_waguri from './profile_waguri.png'
+import profile_rintaro from './profile_rintaro.png'
+import profile_3 from './profile_enrique.png'
+import profile_4 from './profile_3.png'
+import profile_5 from './profile_5.png'
+import search_icon from './search_icon.png'
+import send_button from './send_button.svg'
+import menu_icon from './menu_icon.png'
+import arrow_icon from './arrow_icon.png'
+import code from './code.svg'
+import bgImage from './bgImage.png'
+import pic1 from './pic1.png'
+import pic2 from './pic2.png'
+import pic3 from './pic3.png'
+import pic4 from './pic4.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+
+const assets = {
+    avatar_icon,
+    gallery_icon,
+    help_icon,
+    logo_big,
+    logo_icon,
+    logo,
+    search_icon,
+    send_button,
+    menu_icon,
+    arrow_icon,
+    code,
+    bgImage,
+    profile_waguri,
+}
+
+export default assets;
+
+export const imagesDummyData = [pic1, pic2, pic3, pic4, pic1, pic2]
+
+export const userDummyData = [
+    {
+        "_id": "680f50aaf10f3cd28382ecf2",
+        "email": "test1@osagani.dev",
+        "fullName": "Waguri San",
+        "profilePic": profile_waguri,
+        "bio": "Hi Everyone, I am Using VibeChat",
+    },
+    {
+        "_id": "680f50e4f10f3cd28382ecf9",
+        "email": "test2@osagani.dev",
+        "fullName": "Rinatro Kun",
+        "profilePic": profile_rintaro,
+        "bio": "Hi Everyone, I am Using VibeChat",
+    },
+    {
+        "_id": "680f510af10f3cd28382ed01",
+        "email": "test3@osagani.dev",
+        "fullName": "Subaru Hoshina",
+        "profilePic": profile_3,
+        "bio": "Hi Everyone, I am Using VibeChat",
+    },
+    {
+        "_id": "680f5137f10f3cd28382ed10",
+        "email": "test4@osagani.dev",
+        "fullName": "Saku Natusuwa",
+        "profilePic": profile_4,
+        "bio": "Hi Everyone, I am Using VibeChat",
+    },
+    {
+        "_id": "680f516cf10f3cd28382ed11",
+        "email": "test5@osagani.dev",
+        "fullName": "Shohei Usami",
+        "profilePic": profile_5,
+        "bio": "Hi Everyone, I am Using VibeChat",
+    }
+]
+
+export const messagesDummyData = [
+    {
+        "_id": "680f571ff10f3cd28382f094",
+        "senderId": "680f5116f10f3cd28382ed02",
+        "receiverId": "680f50e4f10f3cd28382ecf9",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "seen": true,
+        "createdAt": "2025-04-28T10:23:27.844Z",
+    },
+    {
+        "_id": "680f5726f10f3cd28382f0b1",
+        "senderId": "680f50e4f10f3cd28382ecf9",
+        "receiverId": "680f5116f10f3cd28382ed02",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "seen": true,
+        "createdAt": "2025-04-28T10:23:34.520Z",
+    },
+    {
+        "_id": "680f5729f10f3cd28382f0b6",
+        "senderId": "680f5116f10f3cd28382ed02",
+        "receiverId": "680f50e4f10f3cd28382ecf9",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "seen": true,
+        "createdAt": "2025-04-28T10:23:37.301Z",
+    },
+    {
+        "_id": "680f572cf10f3cd28382f0bb",
+        "senderId": "680f50e4f10f3cd28382ecf9",
+        "receiverId": "680f5116f10f3cd28382ed02",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "seen": true,
+        "createdAt": "2025-04-28T10:23:40.334Z",
+    },
+    {
+        "_id": "680f573cf10f3cd28382f0c0",
+        "senderId": "680f50e4f10f3cd28382ecf9",
+        "receiverId": "680f5116f10f3cd28382ed02",
+        "image": img1,
+        "seen": true,
+        "createdAt": "2025-04-28T10:23:56.265Z",
+    },
+    {
+        "_id": "680f5745f10f3cd28382f0c5",
+        "senderId": "680f5116f10f3cd28382ed02",
+        "receiverId": "680f50e4f10f3cd28382ecf9",
+        "image": img2,
+        "seen": true,
+        "createdAt": "2025-04-28T10:24:05.164Z",
+    },
+    {
+        "_id": "680f5748f10f3cd28382f0ca",
+        "senderId": "680f5116f10f3cd28382ed02",
+        "receiverId": "680f50e4f10f3cd28382ecf9",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "seen": true,
+        "createdAt": "2025-04-28T10:24:08.523Z",
+    }
+]
